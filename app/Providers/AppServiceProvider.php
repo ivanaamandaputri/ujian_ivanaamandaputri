152e5d3e53563d5dfda20f1d0d67a406f69a5892
ivanaamandaputri <ivanaamandaputri4@gmail.com>
2023-11-24 12:03:26 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        // 
+    }
+}
---
+++ database/seeders/EmployeesTableSeeder.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Employee;
+use Illuminate\Database\Seeder;
+
+class EmployeesTableSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // 
+    }
+}
